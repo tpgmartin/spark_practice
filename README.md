0b47816cfe9466604c3f191c12ee56c5ea0bd9e1
tpgmartin <tpgmartin@gmail.com>
2019-01-07 12:00:38 +0000

    Create labelled points from dataframe, update readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# Spark Practice
+
+This repo contains samples from various Spark tutorials
+
+## Titanic
+
+[Pyspark tutorial](https://creativedata.atlassian.net/wiki/spaces/SAP/pages/83237142/Pyspark+-+Tutorial+based+on+Titanic+Dataset) using Titanic dataset
--- titanic/main.py
+++ titanic/main.py
@@ -2,9 +2,10 @@ import csv
 import numpy as np
 import pyspark
 from pyspark import SparkContext, SparkConf
+from pyspark.ml.feature import VectorAssembler
 from pyspark.mllib.regression import LabeledPoint
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import when
+from pyspark.sql.functions import col, udf, when
 import time
 
 # Not sure if need to specify os config
@@ -12,7 +13,7 @@ conf = pyspark.SparkConf()
 sc = pyspark.SparkContext(conf=conf)
 conf.getAll()
 
-sc.setLogLevel("WARN")
+sc.setLogLevel("ERROR")
 
 # PassengerId, Survived, Pclass, Name                     , Sex , Age, SibSp, Parch, Ticket   , Fare, Cabin, Embarked
 # 1          , 0       , 3     , "Braund, Mr. Owen Harris", male, 22 , 1    , 0    , A/5 21171, 7.25,      , S
@@ -23,9 +24,39 @@ trainTitanic = spark.read.csv("./titanic/data/train.csv", inferSchema=True, head
 
 print("Before --------------------")
 print(trainTitanic.show(5))
+print("Initial DataFrame size", trainTitanic.count())
+print()
 
-# Transform
+# Transform & filter 
 trainTitanic = trainTitanic.withColumn("Sex", when(trainTitanic["Sex"] == "male", 0).otherwise(1))
+# trainTitanic = trainTitanic.filter("Name == ''").filter("Sex == ''")
 
+# Different to example script, feature columns may in different order
 print("After --------------------")
 print(trainTitanic.show(5))
+print("Transformed DataFrame size", trainTitanic.count())
+print()
+
+# Not sure what columns to filter, as columns appear not to be in same order as tutorial
+
+# Creating "labeled point" rdds, something fro MLlib
+# Target label should be "Survived"
+# Features should by "Pclass", "Sex", "Age", "SibSp", "Parch"
+
+# trainTitanicLP=trainTitanic.map(lambda line: LabeledPoint(line["Survived"],[line[["Pclass", "Sex", "Age", "SibSp", "Parch"]]]))
+# print(trainTitanicLP.first())
+
+# Shouldn't need additional transformations as all features are numerical
+assembler = VectorAssembler(
+    inputCols=["Pclass", "Sex", "Age", "SibSp", "Parch"],
+    outputCol="features")
+
+transformedTrain = assembler.transform(trainTitanic)
+
+(transformedTrain.select(col("Survived").alias("label"), col("features"))
+  .rdd
+  .map(lambda row: LabeledPoint(row.label, row.features)))
+
+(trainData, testData) = transformedTrain.randomSplit([0.7, 0.3])
+
+print(testData)
